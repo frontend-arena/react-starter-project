f59c22f9add7b99f9ce13b6e974f0820de0a5cc0
unknown <lambouarnold@gmail.com>
2024-01-04 20:51:25 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import frontendArena from "./frontend-arena-icon.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header flex flex-col items-center justify-center gap-8">
+        <div className="flex items-center justify-center gap-8">
+          <img
+            src={frontendArena}
+            className=""
+            alt="logo"
+            width={128}
+            height={128}
+          />
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+            width={128}
+            height={128}
+          />
+        </div>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
